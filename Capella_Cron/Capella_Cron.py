79b1c1eaad1368929ce89b4caca4f027f6658107
이창진 <54494793+Jotter-Vortex@users.noreply.github.com>
2022-08-25 17:16:55 +0900

    Update Capella_Cron.py

--- Capella_Cron/Capella_Cron.py
+++ Capella_Cron/Capella_Cron.py
@@ -7,22 +7,25 @@ from datetime import datetime
 
 class view():
     def __init__(self):
-        logsize = 0
+        self.sizes = 0
+        self.cnt = 0
+        self.objcontents = []
+        self.logsize = 0
+        
         st.title('Capella CronJob')
-
         option = st.selectbox(
         'Select year',
         ('All', '2020', '2021', '2022'))
 
-        contents, totalsize, cnt = self.capella_size(option)
+        self.capella_size(option)
         st.subheader("summarize")
         st.write( "year :  " + str(option))
-        st.write( "count : " + str(cnt))
-        st.write( "totalsize : " + str(totalsize))
+        st.write( "count : " + str(self.cnt))
+        st.write( "totalsize : " + str(self.sizes))
         st.subheader("samples")
 
         with st.container():
-            for content in contents:
+            for content in self.objcontents:
                 st.code(os.path.basename(content[0]) + "  " + str(content[1]))
 
         today = datetime.today().date()
@@ -35,26 +38,24 @@ class view():
                 elif l != "" and l.split()[0] == "Completed":
                     if l.split("/")[0].split(' ')[2] == l.split("/")[1].split(' ')[1]\
                     and l.split("/")[0].split(' ')[1] == l.split("/")[1].split(' ')[0]:
-                        logsize = l.split("/")[0].split(' ')[2]
+                        self.logsize = l.split("/")[0].split(' ')[2]
                         completed_chk = True
                         break
             if completed_chk == False:
                 st.error("Something's wrong...... check 'aws_s3_log.txt' ")
-            if option == 'All' and logsize < totalsize:
+            if option == 'All' and self.logsize < self.sizes:
                 st.error("new data updated in aws s3... plaese download manualy")
-
-    def capella_size(self,option):
-        sizes, cnt, objcontents = 0, 0, []
-        
-        if option == "All":
-            option = ""
-
-        def sizeof_fmt(num, suffix="B"):
+                
+    def sizeof_fmt(self, num, suffix="B"):
             for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
                 if abs(num) < 1024.0:
                     return f"{num:3.1f} {unit}{suffix}"
                 num /= 1024.0
             return f"{num:.1f}Yi {suffix}"
+
+    def capella_size(self,option):
+        if option == "All":
+            option = ""
 
         resource = boto3.resource('s3')
         resource.meta.client.meta.events.register('choose-signer.s3.*', disable_signing)
@@ -62,9 +63,8 @@ class view():
 
         for obj in bucket.objects.filter(Prefix='data/'+option):
             if 'tiledb/' not in obj.key:
-                if len(objcontents) < 5 and obj.key.split('.')[-1] == 'tif' and '_preview' not in obj.key and obj.key not in objcontents:
-                    objcontents.append([obj.key, sizeof_fmt(obj.size)])
-                sizes += obj.size
-                cnt += 1
-
-        return objcontents, sizeof_fmt(sizes), cnt
+                if len(self.objcontents) < 5 and obj.key.split('.')[-1] == 'tif' and '_preview' not in obj.key and obj.key not in objcontents:
+                    self.objcontents.append([obj.key, self.sizeof_fmt(obj.size)])
+                self.sizes += obj.size
+                self.cnt += 1
+        self.sizes = self.sizeof_fmt(self.sizes)
